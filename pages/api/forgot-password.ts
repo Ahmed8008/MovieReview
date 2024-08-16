8c66f8d96e33a3c21c775fcc37d096406f033f9a
Ahmed8008 <88592717+Ahmed8008@users.noreply.github.com>
2024-08-17 01:59:26 +0500

    Update forgot-password.ts

--- pages/api/forgot-password.ts
+++ pages/api/forgot-password.ts
@@ -29,7 +29,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         service: 'Gmail',
         auth: {
           user: demo7899999@gmail.com,
-          pass: bfapmnypsntylcde
+          pass: bfapmnypsntylcde,
         },
       });
 
